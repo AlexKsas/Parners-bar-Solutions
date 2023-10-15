17a858de9aedbbc1ea5520a357c8532bbedb6201
lennyz0689 <lennyz0689@gmail.com>
2023-10-14 23:58:14 -0500

    register

--- Aplicacion/Events_bar/src/pages/Register/Register.tsx
+++ Aplicacion/Events_bar/src/pages/Register/Register.tsx
@@ -1,10 +1,13 @@
+import { Text } from "react-native"
 import Datos from "../../components/Register/Datos/Datos"
+import React from "react"
 
 const Register = () => {
     return (
-        <view>
+        <React.Fragment>
+            <Text>Hola como estn</Text>
             <Datos />
-        </view>
+        </React.Fragment>
     )
 }
 
