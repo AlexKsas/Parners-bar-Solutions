84e97d621a65fbe1962df44b6e7800b6529e3ad0
lennyz0689 <lennyz0689@gmail.com>
2023-10-14 20:58:51 -0500

    Prueba

--- Aplicacion/Events_bar/src/App.tsx
+++ Aplicacion/Events_bar/src/App.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, } from 'react-native';
 import Login from './components/Login/login';
-import register from './components/register/register';
+import Register from './pages/Register/Register';
 
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={register} />
-      </Stack.Navigator>
-    </NavigationContainer>
+        <React.Fragment>
+          <Register />
+        </React.Fragment>
+
   );
 }
 const styles = StyleSheet.create({
