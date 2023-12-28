b3cba3fe322f94afae40ae23bbea4aa46ebe9ddb
Fercho VM Azure <ferchosalas002@gamil.com>
2023-12-28 04:40:56 +0000

    listo el post de crear establecimientos

--- Backend/app/views.py
+++ Backend/app/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from .models import Establecimientos
+import json
 
 # Create your views here.
 
@@ -29,3 +30,35 @@ def obtener_registros_establecimientos(request):
         registros_json.append(registro_dict)
 
     return JsonResponse({'registros': registros_json})
+
+
+@csrf_exempt
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def crear_registro_establecimientos(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            Rid = data.get('id')
+            RNombreEstablecimiento = data.get('NombreEstablecimiento')
+            RLatitud = data.get('Latitud')
+            RLongitud = data.get('Longitud')
+            RidHorario = data.get('idHorario')
+            
+            # Crear un nuevo registro de establecimiento
+            nuevo_registro = Establecimientos.objects.create(
+                id=Rid,
+                NombreEstablecimiento=RNombreEstablecimiento,
+                Latitud=RLatitud,
+                Longitud=RLongitud,
+                idHorario=RidHorario
+
+            )
+            
+            return JsonResponse({'mensaje': 'Registro creado correctamente'})
+        
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
+
+    else:
+        return JsonResponse({'error': 'Este endpoint solo acepta peticiones POST'})
